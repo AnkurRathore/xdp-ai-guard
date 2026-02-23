97bdca76f93a6fff4538bea5c2c710cd8aefa70b
Ankur Rathore <rathore.ankur@gmail.com>
2026-02-23 11:43:51 +0530

    updated readme

--- xdp-api-guard/src/main.rs
+++ xdp-api-guard/src/main.rs
@@ -36,9 +36,7 @@ async fn main() -> anyhow::Result<()> {
     }
 
     // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
+    // runtime.
     let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/xdp-api-guard"
